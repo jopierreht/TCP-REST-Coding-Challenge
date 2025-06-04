cd95dc0ecb73dd0b47827c08954877b920e3deab
Angel <asolis@kroni.io>
2025-06-04 16:34:11 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Prueba técnica: CRUD de Usuarios con REST API y TCP
+
+- Desarrolla una REST API con un conjunto de funciones CRUD para gestionar usuarios.
+- Sólo está permitido usar módulos nativos de nodejs
+- En el caso del web server, debe ser creado con primiticas TCP.
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Prueba técnica: REST API de Usuarios
- * 
- * Desarrolla una REST API con un conjunto de funciones CRUD para gestionar usuarios,
- * Sólo está permitido usar módulos nativos de nodejs y en el caso del web server, debe
- * ser creado con primiticas TCP.
- */
