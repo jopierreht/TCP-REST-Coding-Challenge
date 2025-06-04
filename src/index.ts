e1b9d91b12490b046f70960a33754c8b548d76e7
Angel <asolis@kroni.io>
2025-06-04 16:27:36 -0600

    fist commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Prueba técnica: REST API de Usuarios
+ * 
+ * Desarrolla una REST API con un conjunto de funciones CRUD para gestionar usuarios,
+ * Sólo está permitido usar módulos nativos de nodejs y en el caso del web server, debe
+ * ser creado con primiticas TCP.
+ */
